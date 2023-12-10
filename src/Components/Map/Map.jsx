b607a488e2c1ac12ffde3778a68d70f1fd172570
RaziaAkbari <akbariharis14@gmail.com>
2023-12-10 19:19:12 +0430

    leaflet installed

--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -1,17 +1,21 @@
 import React from "react"
-
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
+import "leaflet/dist/leaflet.css"
 function Map() {
   return (
-    <>
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d105172.32298620224!2d69.1503104!3d34.537472!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2s!4v1702212442936!5m2!1sen!2s"
-        width="100%"
-        height="100%"
-        style={{ borderRadius: "25px" }}
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"
-      ></iframe>
-    </>
+    <div style={{ height: "100%" }}>
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
   )
 }
 
